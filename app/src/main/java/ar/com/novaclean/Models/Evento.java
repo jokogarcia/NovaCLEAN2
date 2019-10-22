eec0934f5e1dbd38ef36d3539fa56d140d316300
Joaquin Garcia <jokogarcia@gmail.com>
2019-10-22 11:00:39 -0300

    Fixed EventoClass

--- app/src/main/java/ar/com/novaclean/Models/Evento.java
+++ app/src/main/java/ar/com/novaclean/Models/Evento.java
@@ -1,28 +1,12 @@
 package ar.com.novaclean.Models;
 
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.util.Log;
-import android.widget.Toast;
 
-
-
-import com.google.gson.Gson;
 
 import java.io.Serializable;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
-import ar.com.novaclean.Calendario;
-import ar.com.novaclean.MySingleton;
-
-import static android.content.Context.MODE_PRIVATE;
 
 public class Evento implements Serializable {
     public int id;
@@ -31,82 +15,87 @@ public class Evento implements Serializable {
     public String dias;
     public Date fecha;
     public Date fecha_inicio;
-    public int hora_inicio; //x60;
+    public int hora; //x60;
     public int duracion;
     public String tareas_ids;
     public ArrayList<Tarea> Tareas;
     public String empleados_ids;
     public int lugar_id;
     public ArrayList<Empleado> EmpleadosDesignados;
-    public String getHora(){
-        int horas = this.hora_inicio/60;
-        int minutos = this.hora_inicio % 60;
-        return String.format("%02d:%02d",horas,minutos);
-    }
 
-    public String getDias() {
-        if (this.repetible == 1) {
-            ArrayList<String> dias = new ArrayList<>();
-            for (int i = 0; i < this.dias.length(); i++) {
-                char c = this.dias.charAt(i);
-                switch (c) {
-                    case 'L':
-                        dias.add("Lunes");
-                        break;
-                    case 'M':
-                        dias.add("Martes");
-                        break;
-                    case 'X':
-                        dias.add("Miércoles");
-                        break;
-                    case 'J':
-                        dias.add("Jueves");
-                        break;
-                    case 'V':
-                        dias.add("Viernes");
-                        break;
-                    case 'S':
-                        dias.add("Sábado");
-                        break;
-                    case 'D':
-                        dias.add("Domingo");
-                        break;
-                }
-            }
-            if (dias.size() == 1)
-                return dias.get(0);
-            String titulo = "";
-            for (int i = 0; i < dias.size(); i++) {
-                titulo += dias.get(i);
-                if(i == dias.size()-2)
-                    titulo += " y ";
-                else if (i != dias.size() - 1)
-                    titulo += ", ";
-            }
-
-
-            return titulo;
-        } else
-            return (this.fecha.toString());
-
-    }
-    public boolean isOnDate(Date date){
-        boolean response=false;
-
-        if(this.repetible != 0){
-            if(this.dias.contains(dateToL(date)) && this.fecha_inicio.before(date))
-                response = true;
-        }
+    public String getDias(){
+        String retval="";
+        if(this.dias == "LMXJV")
+            retval="Lunes a Viernes";
+        else if(this.dias == "LMXJVS")
+            retval="Lunes a Sábado";
+        else if(this.dias == "LMXJVSD")
+            retval="Todos los días";
+        else if(this.dias == "MXJVS")
+            retval="Martes a Sábado";
+        else if(this.dias == "MXJVSD")
+            retval="Martes a Domingo";
         else{
-            response = this.fecha == date;
+            ArrayList<String> DaysArray = new ArrayList<>();
+            if(this.dias.contains("L"))
+                DaysArray.add("Lunes");
+            if(this.dias.contains("M"))
+                DaysArray.add("Martes");
+            if(this.dias.contains("X"))
+                DaysArray.add("Miércoles");
+            if(this.dias.contains("J"))
+                DaysArray.add("Jueves");
+            if(this.dias.contains("V"))
+                DaysArray.add("Viernes");
+            if(this.dias.contains("S"))
+                DaysArray.add("Sábado");
+            if(this.dias.contains("D"))
+                DaysArray.add("Domingo");
+            if(DaysArray.size() == 0){
+                retval="nunca";
+            }
+            else if(DaysArray.size() == 1){
+                retval=DaysArray.get(0);
+            }
+            else{
+                int i;
+                for(i=0;i<DaysArray.size()-1;i++){
+                    if(i!=0)
+                        retval += ", ";
+                    retval+= DaysArray.get(i);
+                }
+                retval +=" y "+DaysArray.get(i);
+            }
         }
-        return response;
+        return retval;
+
+
+
+
+
     }
-    private String dateToL(Date date){
-        String alldays = " DLMXJVS";
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        return String.valueOf(alldays.charAt(calendar.get(Calendar.DAY_OF_WEEK)));
+
+    public String getHora(){
+        return String.format("%02d:%02d",this.hora/60,this.hora%60);
     }
+
+    public boolean isOnDate(Date time){
+        Calendar dateToCheck = Calendar.getInstance();
+        Calendar EventsDate = Calendar.getInstance();
+        String week="DLMXJVS";
+        String TodaysLetter=String.valueOf(week.charAt(dateToCheck.get(Calendar.DAY_OF_WEEK)-1));
+
+        dateToCheck.setTime(time);
+        EventsDate.setTime(this.fecha);
+        if(this.repetible==0){
+            return dateToCheck.get(Calendar.DAY_OF_YEAR) == EventsDate.get(Calendar.DAY_OF_YEAR) &&
+                    dateToCheck.get(Calendar.YEAR) == EventsDate.get(Calendar.YEAR);
+        }
+        else
+        {
+            return (this.dias.contains(TodaysLetter) && this.fecha_inicio.before(time));
+        }
+    }
+
 
 }
